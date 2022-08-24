25fe5a8c656d5abe366a0858e360e7b15dff8e8b
Rahul jain <rahul.jain@w3villa.com>
2022-08-24 09:54:59 +0530

    adding some files and footer

---
+++ main.js
@@ -0,0 +1,8 @@
+nav = document.querySelector('.nav');
+toggleBtn = document.querySelector('.toggle-btn');
+
+
+toggleBtn.addEventListener('click', function(){
+   nav.classList.toggle('collapse-toggle')
+})
+
